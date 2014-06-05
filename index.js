4586c616ea3fefdce251859b32c580d914259e2f
Sean Hickey <shickey.dev@gmail.com>
2014-06-05 09:22:43 -0500

    Routing set up

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require("express");
+var jade = require("jade");
 var path = require("path");
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.get('/', function(req, res) {
+  res.render('shoes');
+});
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
