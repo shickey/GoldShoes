a436ba3873fcb0a596f70e7eeb9d8e95683bef63
Sean Hickey <shickey.dev@gmail.com>
2014-06-05 09:36:31 -0500

    Images added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var jade = require("jade");
 var path = require("path");
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.get('/', function(req, res) {
